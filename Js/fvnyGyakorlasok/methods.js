34b1676055b370167c9ca1706b2c5e25e9534ace
Chris <chrismac@Chriss-MacBook-Air.local>
2023-09-13 17:03:07 +0200

    fileName_changes

--- Js/fvnyGyakorlasok/methods.js
+++ Js/fvnyGyakorlasok/methods.js
@@ -2,6 +2,7 @@
 //Készíts egy olyan függvényt, ami egy adott paraméterként kapott szövegből minden második karaktert törli. 
 
 let peldaSzoveg = 'Szöveg a példa kedvéért';
+document.body.innerHTML += `<h1>${peldaSzoveg}</h1>`
 function csakaFele(szoveg) {
     let ujSzoveg = '';
     for (let i = 0; i < szoveg.length; i += 2) {
@@ -9,10 +10,15 @@ function csakaFele(szoveg) {
     }
     return ujSzoveg;
 }
-document.body.innerHTML += csakaFele(peldaSzoveg);
-
-//Tabusító
-//Készíts egy olyan függvényt, ami egy adott paraméterként kapott szövegből bizonyos szintén paraméterként kapott karaktert töröl.
+document.body.innerHTML += '<h3>1. Feladat </h3>' + csakaFele(peldaSzoveg) + '<br>';
 
 //Magánhangzó tabusító
 //Készíts egy olyan függvényt, ami egy adott paraméterként kapott szövegből törli az összes magánhangzót.
+
+function maganHangzoTorlo(peldaSzoveg) {
+    return peldaSzoveg.replace(/[aáeéiíoóöőuúüűAÁEÉIÍOÓÖŐUÚÜŰ]/g, "");
+}
+
+let eredmeny = maganHangzoTorlo(peldaSzoveg);
+
+document.body.innerHTML += '<h3>2. Feladat </h3>' + eredmeny + '<br>';
